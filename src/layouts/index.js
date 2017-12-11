961f190ff962ee2a456c080f7679c676c10effab
Andrew Jacobs <andrew@flyingtrolleycars.com>
2017-12-10 19:03:11 -0800

    Updated header to use slanted hero image

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,34 +3,27 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-import './index.css'
+import './index.scss'
 
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Seattle Developer&apos;s Co-op
-        </Link>
-      </h1>
-    </div>
+  <div className="hero">
+    <figure>
+      <img src="https://unsplash.it/1600/900?image=830" alt="" />
+      <figcaption>
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: 'white',
+              textDecoration: 'none',
+            }}
+          >
+            Seattle Developer&apos;s Co-op
+          </Link>
+        </h1>
+        <p>Image with CSS clip-path</p>
+      </figcaption>
+    </figure>
   </div>
 )
 
